9c4e18954965e0977bda35a790ccf2dc9b1ca8f4
Suman Kumar <suman.k@proximity.tech>
2021-07-30 01:37:49 +0530

    UX fixed for home page for all devices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,19 +29,19 @@ export default function Home({ res }: { res: ProductI }) {
         <>
           <div className="w-full z-30 top-0 pb-1">
             <section
-              className="w-full mx-auto bg-nordic-gray-light flex pt-12 md:pt-0 md:items-center bg-cover bg-right xl h-98"
+              className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right"
               style={{
                 backgroundImage: `url(https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80)`,
               }}
             >
-              <div className="container mx-auto h-96">
-                <div className="flex flex-col w-full lg:w-1/2 justify-center items-start  px-6 tracking-wide">
+              <div className="container mx-auto md:h-96 lg:h-96">
+                <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
                   <h1 className="text-black text-2xl my-4">Stripy Zig Zag Jigsaw Pillow and Duvet Set</h1>
                   <a
-                    className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black"
-                    href="/"
+                    className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black mb-4"
+                    href="/shop"
                   >
-                    products
+                    Explore Products
                   </a>
                 </div>
               </div>
