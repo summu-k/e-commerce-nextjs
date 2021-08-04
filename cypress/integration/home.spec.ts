c01b89ccfc3f7c5a4bff6b603c85c6a08e57100c
Suman Kumar <suman.k@proximity.tech>
2021-08-04 22:10:32 +0530

    Cypress test added for mobile

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -3,11 +3,21 @@
 
 describe('Visit homepage and load all products', () => {
   it('Visit e-commerce home page', () => {
+    cy.viewport(1280, 720);
     cy.visit('/');
     cy.contains('Store');
     cy.get('[data-test-py="productListing"]').should('exist');
     cy.scrollTo('bottom', { ensureScrollable: true, duration: 2000 });
     cy.get('[data-test-py="footerBottom"]').should('have.text', 'About');
+
+    // cy.visit(Cypress.env('productListingUrl'));
+  });
+
+  it('Visit home page and verify footer is visible on mobile', () => {
+    cy.viewport('iphone-x');
+    cy.visit('/');
+    cy.scrollTo('bottom', { ensureScrollable: true, duration: 2000 });
+    cy.get('[data-test-py="footerBottom"]').should('be.visible');
 
     // cy.visit(Cypress.env('productListingUrl'));
   });
